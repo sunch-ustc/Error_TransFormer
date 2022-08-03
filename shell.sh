cfcfbc395dc582211f57e36e290251faf71b9590
sunch-ustc <96180792+sunch-ustc@users.noreply.github.com>
2022-08-03 19:54:53 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,17 +7,17 @@ attack_method           # options ETF_PGD  or  Deep_PGD
 lightweight_model      # The location of model
 """
 
-"""Train_model"""
-# python /home/common/sunch/Error_TransFormer_bithub/Train_model.py --img_sum 1000   --img_num 1 --backbone resnet10 --batch_size 1000 \
+"""Step one : Train_model"""
+# python /home/common/sunch/Error_TransFormer/Train_model.py --img_sum 1000   --img_num 1 --backbone resnet10 --batch_size 1000 \
 #                     --attack ETF-I \
-#                     --root_path  /home/common/sunch/Error_TransFormer_bithub   \
-#                     --lightweight_model  /home/common/sunch/Error_TransFormer_bithub/results/ETF-I/I_test.pth.tar  --seed 1  \
+#                     --root_path  /home/common/sunch/Error_TransFormer   \
+#                     --lightweight_model  /home/common/sunch/Error_TransFormer/results/ETF-I/I_test.pth.tar  --seed 1  \
 #                     --data_dir             /home/common/sunch/ILSVRC2012_img_val
 
-"""ETF_Attack"""
-python /home/common/sunch/Error_TransFormer_bithub/ETF_Attack.py --backbone resnet10  --img_sum 500   --img_num 1 --batch_size 100 \
-                --root_path  /home/common/sunch/Error_TransFormer_bithub   \
-                --lightweight_model  /home/common/sunch/Error_TransFormer_bithub/results/ETF-I/I-1.pth.tar   --seed 1  \
+"""Step two : ETF_Attack"""
+python /home/common/sunch/Error_TransFormer/ETF_Attack.py --backbone resnet10  --img_sum 1000   --img_num 1 --batch_size 100 \
+                --root_path  /home/common/sunch/Error_TransFormer   \
+                --lightweight_model  /home/common/sunch/Error_TransFormer/results/ETF-I/I-1.pth.tar   --seed 1  \
                 --data_dir                /home/common/sunch/ILSVRC2012_img_val         \
                 --attack_method ETF_PGD
  
